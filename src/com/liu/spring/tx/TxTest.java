fcfb58a576103c5a960a386d306b5abcffc4ab9d
liushuaibiao <misteryliu@outlook.com>
2023-03-14 22:20:52 +0800

    spring下完结：事务的隔离级别

--- src/com/liu/spring/tx/TxTest.java
+++ src/com/liu/spring/tx/TxTest.java
@@ -8,23 +8,23 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TxTest {
 
-//    测试根据id查询商品
+    //    测试根据id查询商品
     @Test
-    public void queryPriceByIdTest(){
+    public void queryPriceByIdTest() {
         ClassPathXmlApplicationContext ioc = new ClassPathXmlApplicationContext("tx_ioc.xml");
         GoodsDao goodsDao = ioc.getBean(GoodsDao.class);
         Float price = goodsDao.queryPriceById(1);
-        System.out.println("id="+1+"的商品价格="+price);
+        System.out.println("id=" + 1 + "的商品价格=" + price);
     }
 
     /*
     根据id修改用户余额
      */
     @Test
-    public void updateBalanceTest(){
+    public void updateBalanceTest() {
         ClassPathXmlApplicationContext ioc = new ClassPathXmlApplicationContext("tx_ioc.xml");
         GoodsDao goodsDao = ioc.getBean(GoodsDao.class);
-       goodsDao.updateBalance(1,1.0F);
+        goodsDao.updateBalance(1, 1.0F);
         System.out.println("改变余额成功");
     }
 
@@ -32,39 +32,57 @@ public class TxTest {
     根据商品id修改商品库存
      */
     @Test
-    public void updateAmountTest(){
+    public void updateAmountTest() {
         ClassPathXmlApplicationContext ioc = new ClassPathXmlApplicationContext("tx_ioc.xml");
         GoodsDao goodsDao = ioc.getBean(GoodsDao.class);
-        goodsDao.updateAmount(1,1);
+        goodsDao.updateAmount(1, 1);
         System.out.println("改变商品库存");
     }
 
     //测试用户购买商品业务
     @Test
-    public void byGoodTest(){
+    public void byGoodTest() {
         ClassPathXmlApplicationContext ioc = new ClassPathXmlApplicationContext("tx_ioc.xml");
         GoodsService goodsService = ioc.getBean(GoodsService.class);
-        goodsService.buyGoods(1,1,1);
+        goodsService.buyGoods(1, 1, 1);
         System.out.println("商品购买成功");
     }
 
     //测试用户带有事务的购买商品业务
     @Test
-    public void byGoodTxTest(){
+    public void byGoodTxTest() {
         ClassPathXmlApplicationContext ioc = new ClassPathXmlApplicationContext("tx_ioc.xml");
         GoodsService goodsService = ioc.getBean(GoodsService.class);
-        goodsService.buyGoodsTx(1,1,1);
+        goodsService.buyGoodsTx(1, 1, 1);
         System.out.println("商品购买成功");
     }
 
 
-//    测试事务的传播机制
+    //    测试事务的传播机制
     @Test
-    public void MultiBuyGoodsByTx(){
+    public void MultiBuyGoodsByTx() {
         ClassPathXmlApplicationContext ioc = new ClassPathXmlApplicationContext("tx_ioc.xml");
         GoodsService goodsService = ioc.getBean(GoodsService.class);
         MultiplyService multiplyService = ioc.getBean(MultiplyService.class);
         multiplyService.MultiBuyGoodsByTx();
+    }
+
+    //    测试默认事务隔离级别：可重复读
+    @Test
+    public void IsolationLevel() {
+        ClassPathXmlApplicationContext ioc = new ClassPathXmlApplicationContext("tx_ioc.xml");
+        GoodsService goodsService = ioc.getBean(GoodsService.class);
+        goodsService.buyGoodsIsolationLevel(1);
+    }
+
+
+    //    测试事务超时回滚
+    @Test
+    public void byGoodTimeTxTest() {
+        ClassPathXmlApplicationContext ioc = new ClassPathXmlApplicationContext("tx_ioc.xml");
+        GoodsService goodsService = ioc.getBean(GoodsService.class);
+        goodsService.buyGoodsTimeTx(1, 1, 1);
+        System.out.println("商品购买成功");
     }
 
 
--- src/com/liu/spring/tx/service/GoodsService.java
+++ src/com/liu/spring/tx/service/GoodsService.java
@@ -2,6 +2,7 @@ package com.liu.spring.tx.service;
 
 import com.liu.spring.tx.dao.GoodsDao;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,14 +13,15 @@ public class GoodsService {
     //定义属性GoodsDao
     @Resource
     private GoodsDao goodsDao;
-//    编写一个方法，完成用户购买商品的业务
-    public void buyGoods(int userId,int goodId,int amount){
+
+    //    编写一个方法，完成用户购买商品的业务
+    public void buyGoods(int userId, int goodId, int amount) {
         //得到商品的价格
         Float price = goodsDao.queryPriceById(userId);
         //减少用户的余额
-        goodsDao.updateBalance(userId,price*amount);
+        goodsDao.updateBalance(userId, price * amount);
         //减少库存量
-        goodsDao.updateAmount(goodId,amount);
+        goodsDao.updateAmount(goodId, amount);
     }
 
     /*
@@ -27,15 +29,38 @@ public class GoodsService {
     使用@Transactional 可以进行声明式事务
      */
     @Transactional(propagation = Propagation.REQUIRES_NEW)
-    public void buyGoodsTx(int userId,int goodId,int amount){
+    public void buyGoodsTx(int userId, int goodId, int amount) {
         //得到商品的价格
         Float price = goodsDao.queryPriceById(userId);
         //减少用户的余额
-        goodsDao.updateBalance(userId,price*amount);
+        goodsDao.updateBalance(userId, price * amount);
         //减少库存量
         try {
-            goodsDao.updateAmount(goodId,amount);
-        }catch (Exception e){
+            goodsDao.updateAmount(goodId, amount);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+//    事务超时回滚
+//    没有设置timeout默认=-1.表示使用默认的事务超时时间
+    @Transactional(timeout = 2)
+    public void buyGoodsTimeTx(int userId, int goodId, int amount) {
+        //得到商品的价格
+        Float price = goodsDao.queryPriceById(userId);
+        //减少用户的余额
+        goodsDao.updateBalance(userId, price * amount);
+//        在这模拟超时，看上面的修改是否会回滚
+        try {
+            Thread.sleep(4000);
+            System.out.println("超时结束");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        //减少库存量
+        try {
+            goodsDao.updateAmount(goodId, amount);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -45,12 +70,24 @@ public class GoodsService {
     演示事务的传播机制
      */
     @Transactional(propagation = Propagation.REQUIRES_NEW)
-    public void buyGoodsTx2(int userId,int goodId,int amount){
+    public void buyGoodsTx2(int userId, int goodId, int amount) {
         //得到商品的价格
         Float price = goodsDao.queryPriceById2(userId);
         //减少用户的余额
-        goodsDao.updateBalance2(userId,price*amount);
+        goodsDao.updateBalance2(userId, price * amount);
         //减少库存量
-        goodsDao.updateAmount2(goodId,amount);
+        goodsDao.updateAmount2(goodId, amount);
+    }
+
+    //    演示事务的默认隔离级别：可重复读
+//    在第一次读取之后，中间会在数据库中修改数据，但是读取的结果是一致的。
+    @Transactional(isolation = Isolation.READ_COMMITTED)
+    public void buyGoodsIsolationLevel(int goodId) {
+        //得到商品的价格
+        Float price1 = goodsDao.queryPriceById2(goodId);
+        System.out.println("第一次读取的商品价格："+price1);
+//        可重复读取商品价格
+        Float price2 = goodsDao.queryPriceById2(goodId);
+        System.out.println("第二次读取的商品价格："+price2);
     }
 }
